0e7e22e822b68e1e4cc32cb7bb9a29932c259f28
Alex Senik <alex.senik@samposoftware.com>
2017-04-25 15:46:40 +0300

    Fix comments

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -18,8 +18,8 @@ module Api
 
     def pagination_dict(collection)
       hash = { count: collection.total_pages }
-      hash[:next_url] = api_v1_cms_materials_path(page: collection.next_page) if collection.next_page.present?
-      hash[:prev_url] = api_v1_cms_materials_path(page: collection.previous_page) if collection.previous_page.present?
+      hash[:next_url] = api_v1_cms_materials_url(page: collection.next_page) if collection.next_page.present?
+      hash[:prev_url] = api_v1_cms_materials_url(page: collection.previous_page) if collection.previous_page.present?
       hash
     end
   end
