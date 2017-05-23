2ab8bd77a98a68474a83c11a66c472fd418eb18c
Shaliko Usubov <shaliko@ezid.ru>
2017-05-23 19:09:36 +0300

    Format code.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       namespace :cms do
-        resources :materials
+        resources :materials, only: %i[index show create update destroy]
         resources :metadata, only: %i[index show]
 
         resources :apidocs, only: %i[index show]
